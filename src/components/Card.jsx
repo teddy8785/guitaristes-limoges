777da2cff903905b0354721081d87d62a4b436ab
teddy8587 <teddy.barieraud@gmail.com>
2024-11-29 19:08:41 +0100

    feat condition affichage lecteur audio + style responsive

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,26 +2,27 @@ import { NavLink } from "react-router-dom";
 import "../styles/card.css";
 
 function Card({ id, nom, photo, audio }) {
-    return(
+  return (
     <article className="card">
       <p className="card__name">{nom}</p>
       <NavLink className="card__link" to={`/Artiste/${id}`}>
-      <picture>
-        {/* <source
+        <picture>
+          {/* <source
             srcSet={`${process.env.PUBLIC_URL}/${photoDown}`}
             media="(max-width: 768px)"
           /> */}
-        <img
-        className="card__img"
-          src={`${process.env.PUBLIC_URL}/${photo}`}
-          alt={nom}
-        />
-      </picture>
-      <audio controls className="card__audio">
-        <source src={audio} />
-      </audio>
+          <img
+            className="card__img"
+            src={`${process.env.PUBLIC_URL}/${photo}`}
+            alt={nom}
+          />
+        </picture>
+        {audio && audio.length > 0 && (
+          <audio controls className="card__audio">
+            <source src={audio} />
+          </audio>
+        )}
       </NavLink>
-      
     </article>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,14 +4,11 @@ import data from "../artistes.json";
 import "../styles/main.css";
 
 function Main() {
-
   const maxCards = 4;
 
   return (
     <main className="main">
-      <h2 className="main__title">
-        NOUVEAU
-      </h2>
+      <h2 className="main__title">NOUVEAU</h2>
       <div className="main__gallery">
         {data.slice(-maxCards).map((post) => (
           <Card
