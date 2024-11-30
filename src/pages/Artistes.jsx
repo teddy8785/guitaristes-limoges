7cb16a623376059624af20bbc9a17fde1046f0e5
teddy8587 <teddy.barieraud@gmail.com>
2024-11-30 07:15:13 +0100

    feat ajout du scroll auto au en haut de page artistes

--- src/pages/Artistes.jsx
+++ src/pages/Artistes.jsx
@@ -1,17 +1,22 @@
 import Header from "../components/Header";
 import Gallery from "../components/Gallery";
 import { NavLink } from "react-router-dom";
+import { useEffect } from "react";
 
 function Artistes() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div>
       <Header>
         <nav>
-        <NavLink className="main__link" style={{margin:0}} to={`/Index`}>
-        Retour
-      </NavLink>
-      </nav>
-      <h1 className="header__title">GUITARISTES LIMOGES</h1>
+          <NavLink className="main__link" style={{ margin: 0 }} to={`/Index`}>
+            Retour
+          </NavLink>
+        </nav>
+        <h1 className="header__title">GUITARISTES LIMOGES</h1>
       </Header>
       <Gallery />
     </div>
