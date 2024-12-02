57d973f332ff180d16c3cb04e276ce3d3f81df05
teddy8587 <teddy.barieraud@gmail.com>
2024-12-02 09:01:33 +0100

    feat toggleMenu + component contact form

---
+++ src/components/Contact.jsx
@@ -0,0 +1,57 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "../styles/contact.css";
+
+export const ContactUs = () => {
+  const form = useRef();
+
+  const SERVICE_ID = 'service_fg2ovcs';
+  const TEMPLATE_ID = 'template_gs4rnjf';
+  const PUBLIC_KEY = 'KUsWFUTSrViBBXM6W';
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+    .sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY)
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+      e.target.reset()
+  };
+
+  return (
+    <form className="contact__form" ref={form} onSubmit={sendEmail}>
+      <label htmlFor="nom">Ecrivez votre nom :</label>
+      <input type="text" className="contact__input" id="nom" name="nom" required></input>
+      <br />
+      <label htmlFor="mail">Ecrivez votre email :</label>
+      <input type="text" className="contact__input" id="mail" name="email" required></input>
+      <br />
+      <label htmlFor="objet">Ecrivez l'objet de votre message :</label>
+      <input type="text" className="contact__input" id="objet"  name="objet" required></input>
+      <br />
+      <label htmlFor="msg">Ecrivez votre message :</label>
+      <textarea className="contact__msg" id="msg" name="message" required></textarea>
+      <br />
+      <button type="submit" className="contact__button">
+        Envoyer
+      </button>
+    </form>
+  );
+};
+
+function Contact() {
+  return (
+    <section id="contact" className="contact">
+      <h2 className="contact__title">CONTACTER :</h2>
+      <ContactUs />
+    </section>
+  );
+}
+
+export default Contact;
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,22 +1,48 @@
 import Header from "../components/Header";
 import Main from "../components/Main";
 import Card from "../components/Card";
+import Contact from "../components/Contact";
 import data from "../artistes.json";
 import { NavLink } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+import { useState } from "react";
 
 function Index() {
   const maxCards = 4;
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    console.log('ok');
+    
+  }
+
   return (
     <div>
       <Header>
-        <NavLink className="main__link" to={`/Gallery`}>
+        <i className="fa-solid fa-bars" onClick={toggleMenu}></i>
+        <NavLink className="header__link" to={`/Gallery`}>
           ...Voir tous les guitaristes
         </NavLink>
+        <nav className={`header__ancre--container ${isMenuOpen ? "" : "header__ancre--hidden"}`}>
+          <HashLink
+            smooth
+            to="#new"
+            className="header__ancre header__ancre--scroll"
+          >
+            Nouveau
+          </HashLink>
+          <HashLink smooth to="#contact" className="header__ancre">
+            contact
+          </HashLink>
+        </nav>
         <h1 className="header__title">GUITARISTES LIMOGES</h1>
       </Header>
       <Main>
-        <h2 className="main__title">NOUVEAU</h2>
+        <section id="new" className="main__">
+        <h2 className="main__title">
+          NOUVEAU
+        </h2>
         <div className="main__gallery">
           {data.slice(-maxCards).map((post) => (
             <Card
@@ -28,7 +54,10 @@ function Index() {
             />
           ))}
         </div>
+        </section>
+        <Contact />
       </Main>
+     
     </div>
   );
 }
