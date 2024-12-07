113fd668f5b4f49b7c3f680dd2d41a077d4bc0a3
teddy8587 <teddy.barieraud@gmail.com>
2024-12-07 15:09:30 +0100

    perf create component pagination

--- src/pages/Artistes.jsx
+++ src/pages/Artistes.jsx
@@ -1,37 +1,37 @@
 import Header from "../components/Header";
 import Main from "../components/Main";
 import Card from "../components/Card";
+import Pagination from "../components/Pagination";
 import data from "../artistes.json";
 import { useEffect, useState } from "react";
 
 function Artistes() {
-
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-const maxCards = 20;
-const totalPages = Math.ceil(data.length / maxCards);
-const [currentPage, setCurrentPages] = useState(1);
+  const maxCards = 20;
+  const totalPages = Math.ceil(data.length / maxCards);
+  const [currentPage, setCurrentPages] = useState(1);
 
-const getCurrentPageData = () => {
-  const startIndex = (currentPage - 1) * maxCards;
-  const endIndex = startIndex + maxCards;
-  return data.slice(startIndex,endIndex);
-}
+  const getCurrentPageData = () => {
+    const startIndex = (currentPage - 1) * maxCards;
+    const endIndex = startIndex + maxCards;
+    return data.slice(startIndex, endIndex);
+  };
 
-const changePage = (page) => {
-  if (page > 0 && page <= totalPages) {
-    setCurrentPages(page);
-    window.scrollTo(0, 0);
-  }
-}
+  const changePage = (page) => {
+    if (page > 0 && page <= totalPages) {
+      setCurrentPages(page);
+      window.scrollTo(0, 0);
+    }
+  };
 
   return (
     <div>
       <Header>
-        <nav>
-        <button
+        <nav className="header__nav">
+          <button
             className="header__button"
             href="#"
             onClick={(e) => {
@@ -46,20 +46,11 @@ const changePage = (page) => {
       </Header>
       <Main>
         <h2 className="main__title">TOUS LES GUITARISTES</h2>
-        <div className="main__pagination">
-        {Array.from({ length: totalPages }, (_, index) => (
-          <button
-            key={index}
-            className={`main__pagination--button ${
-              currentPage === index + 1 ? "active" : ""
-            }`}
-            onClick={() => changePage(index + 1)}
-            disabled={currentPage === index + 1}
-          >
-            {index + 1}
-          </button>
-        ))}
-      </div>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={changePage}
+        />
         <div className="main__gallery">
           {getCurrentPageData().map((post) => (
             <Card
@@ -73,23 +64,12 @@ const changePage = (page) => {
             />
           ))}
         </div>
-        <div className="main__pagination">
-        {Array.from({ length: totalPages }, (_, index) => (
-          <button
-            key={index}
-            className={`main__pagination--button ${
-              currentPage === index + 1 ? "active" : ""
-            }`}
-            onClick={() => changePage(index + 1)}
-            disabled={currentPage === index + 1}
-          >
-            {index + 1}
-          </button>
-        ))}
-      </div>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={changePage}
+        />
       </Main>
-
-     
     </div>
   );
 }
