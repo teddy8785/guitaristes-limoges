d932259a70185519d66d793e80bfb157ba7569fd
teddy8587 <teddy.barieraud@gmail.com>
2025-05-25 23:08:17 +0200

    modif footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "../styles/footer.css";
 function Footer() {
     return (
 <footer className="footer">
-<p className="footer__copyright">&copy; 2024 teddy barieraud - guitaristes limoges</p>
+<p className="footer__copyright">&copy; 2024 - guitaristes limoges</p>
 </footer>
     );
 };
